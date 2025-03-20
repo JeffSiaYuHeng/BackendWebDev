f49687ece69132492ed16367460ae48ef1871f04
moyasuxin <moyasuxin@gmail.com>
2025-03-20 08:53:58 +0800

    update css where index cannot connect to css file somehow

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ include "backend/index.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="style/index.css">
     <script src="userscript/transition.js"></script>
     <link rel="stylesheet" href="userstyle/transition.css">
     <title>Welcome to Eternal Elegant Bridal</title>
